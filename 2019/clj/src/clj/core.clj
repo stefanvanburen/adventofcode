ac1ed972c891ad884564864b4bd51c0e1d43db44
Stefan VanBuren <vanburenstefan@gmail.com>
2019-12-04 00:57:44 -0500

    Add day 4; not ready to make my horrific day 3 known

--- 2019/clj/src/clj/core.clj
+++ 2019/clj/src/clj/core.clj
@@ -1,5 +1,6 @@
 (ns clj.core)
 
+; day one
 (def values
   (->>
     (slurp "one.txt")
@@ -25,6 +26,7 @@
 (def one-two (int (reduce + (map #(reduce + %) (map fuel-recur values)))))
 (= one-two 4736213)
 
+; day two
 (def values (vec (map read-string (clojure.string/split (slurp "two.txt") #","))))
 
 (def start-values (assoc (assoc values 2 2) 1 12) )
@@ -47,3 +49,35 @@
 (doseq [noun (range 100) verb (range 100)]
   (if (= ((parse (assoc (assoc values 2 verb) 1 noun) 0) 0) 19690720)
     (prn (+ (* 100 noun) verb))))
+; day 4
+
+(def input (range 307237 769058))
+
+(defn always-increasing [x]
+  (= (map int (seq (str x))) (sort < (map int (seq (str x))))))
+
+(defn has-adjacent-match [x]
+  (not (= (seq x) (dedupe x))))
+
+; part 1
+(count
+  (filter
+    #(and
+       (always-increasing %)
+       (has-adjacent-match (str %))
+    input))
+
+; part 2
+(count
+  (filter
+    #(and
+       (always-increasing %)
+       (has-adjacent-match (str %))
+       (has-two-count (combine-adjacent (str %))))
+    input))
+
+
+(defn combine-adjacent [x]
+  (->> (partition-by identity x)))
+
+(defn has-two-count [x] (some #(= (count %) 2) x))
